8cb165afa6bf5b88a854e80ddd97ebe5f0d093c4
nobuyori-gh <vcd02nm120jtik@gmail.com>
2020-05-24 16:38:05 +0900

    [Update]migrate email追記

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,12 @@ ActiveRecord::Schema.define(version: 2020_05_17_115338) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "introduction"
